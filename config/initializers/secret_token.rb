a8681a85b835de36bf34498acb59617a0d4d01c3
tdouglas <tdouglas@seamlessTL.com>
2013-08-02 16:59:57 -0400

    first commit on 2nd project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeArt::Application.config.secret_token = 'c2c66cf44f19247f196e42cc77a1d00077245366e678f8c463b1da29f9352c151dd010488fcb443e3363f7f74db42ff1b09fbc444fce9f06bd005a53e9823ff7'
