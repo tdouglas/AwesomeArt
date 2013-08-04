87f0bd52becd3813c3107b1ad3e279aab91d576f
tdouglas <tdouglas@seamlessTL.com>
2013-08-04 00:42:53 -0400

    array of images appended to div

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,27 +16,27 @@
 
 
 // zoom in on div
-$('#page-wrap').on('click',function(){
+$('.page-wrap').on('click',function(){
   var step = 2;
   currIEZoom += step;
-  $('#page-wrap').css('zoom', ' ' + currIEZoom + '%');
+  $('.page-wrap').css('zoom', ' ' + currIEZoom + '%');
 });
 
 
-hide = $('#page-wrap').hide();
-show = $('#page-wrap').show();
+hide = $('.page-wrap').hide();
+show = $('.page-wrap').show();
 
 // slide down
 $(document.body).hover(function () {
-  if ($('#page-wrap').is(":hidden")) {
-  $('#page-wrap').slideDown("slow");
+  if ($('.page-wrap').is(":hidden")) {
+  $('.page-wrap').slideDown("slow");
   } else {
-  $('#page-wrap').hide();
+  $('.page-wrap').hide();
   }
 });
 
 $(document.body).click(function () {
-  $('#page-wrap').slideDown("slow");
+  $('.page-wrap').slideDown("slow");
 });
 
 // slide up
@@ -49,6 +49,53 @@ $(document.body).click(function () {
   });
 
 $(function(){
-  $('#page-wrap').hide();
-  $('#page-wrap').slideDown(2100);
+  $('.page-wrap').hide();
+  $('.page-wrap').slideDown(2100);
 });
+
+function fitImagetoContainer() {
+  $("img").each(function(i) {
+     $(this).width($(this).parent().width());
+  });
+}
+$(window).resize(function() {
+   fitImagetoContainer();
+});
+
+$('.page-wrap').prepend('<img src="assets/bg14.jpg" />');
+
+
+// can this be a function that pulls photos from assets folder directly
+imageArray = [
+  '<img src="assets/bg1.jpg"/>',
+  '<img src="assets/bg2.jpg"/>',
+  '<img src="assets/bg3.jpg"/>',
+  '<img src="assets/bg4.jpg"/>',
+  '<img src="assets/bg5.jpg"/>',
+  '<img src="assets/bg6.jpg"/>',
+  '<img src="assets/bg7.gif"/>',
+  '<img src="assets/bg8.jpg"/>',
+  '<img src="assets/bg9.jpg"/>',
+  '<img src="assets/bg10.jpg"/>',
+  '<img src="assets/bg11.jpg"/>',
+  '<img src="assets/bg12.jpg"/>',
+  '<img src="assets/bg13.jpg"/>',
+  '<img src="assets/bg15.jpg"/>'
+];
+
+// can this be stored in a function as well and shuffle so the pictures shuffle in a unique order
+for (var i = 0; i < imageArray.length; i++) {
+  $('.page-wrap').append(imageArray);
+}
+
+$('img').each(function(){
+  if ($(this).width() > $(this).height()){
+        $(this).addClass('inner-image');
+  } else {
+        $(this).addClass('inner-image');
+  }
+});
+
+
+
+
