806e644bf84ead983355ea52f560d8a7c2210568
mvanger <mvanger@nyu.edu>
2013-08-05 20:40:01 -0400

    Set up model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessible :screenname, :email, :password, :password_confirmation
+
+  has_and_belongs_to_many :artists
+  has_and_belongs_to_many :artworks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130805180804) do
-
-  create_table "artists", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "artworks", :force => true do |t|
-    t.string   "title"
-    t.string   "date"
-    t.string   "medium"
-    t.string   "collection"
-    t.string   "image_url"
-    t.string   "museum_page_url"
-    t.integer  "image_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "screenname"
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
